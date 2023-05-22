bf17985cdc9bb659f51fa587007abadf6ba57d17
Konan Wang <konanwang@gmail.com>
2023-05-22 15:39:45 +0800

    Basic Version-2

--- guess_num.py
+++ guess_num.py
@@ -4,7 +4,9 @@
 #猜錯了, 要告訴他比答案大還是小
 import random #創造一個隨機整數，載入這個標準含式庫(一本書)
 r = random.randint(1, 100) #在random這標準函式庫(這本書)挑出一個功能randint，函式是1~100
+count = 0 # 因為要記錄猜幾次，起始值就一定要放在迴圈外
 while True: #所有只要是真實的，就需要一直的執行此迴圈
+	count = count + 1 #記錄猜了幾次
 	num = input('請輸入數字:') #注意在input裡，系統最後會把它視為一個字串，因此需要函式轉換成一個Number
 	num = int(num)
 	if num == r:
@@ -14,3 +16,4 @@ while True: #所有只要是真實的，就需要一直的執行此迴圈
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('這是你猜的第幾次', count, '次')
